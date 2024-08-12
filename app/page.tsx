4cf0016c69a7928f26dca723647afda3bfc124fc
movlan <bmovlanov@gmail.com>
2024-08-11 21:46:20 -0400

    home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
+import PatientForm from "@/components/form/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
-      <Button>Button</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PassKey Modal */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={1000}
+            height={1000}
+            className="mb-1 h10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2022. All rights reserved
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/onboarding-img.png"}
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
